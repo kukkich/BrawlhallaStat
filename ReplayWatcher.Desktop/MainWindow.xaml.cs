4ccb851074de628058cc6b64d446ae5a289f6487
kukkich <vitia.iezhovqqqq@gmail.com>
2024-02-01 13:54:21 +0700

    Добавил DI и настроил поведение при сворачивании и скрытии

--- ReplayWatcher.Desktop/MainWindow.xaml.cs
+++ ReplayWatcher.Desktop/MainWindow.xaml.cs
@@ -2,89 +2,77 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
-namespace ReplayWatcher.Desktop
+namespace ReplayWatcher.Desktop;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private readonly TaskbarIcon _taskbar;
+    private bool Hidden => !ShowInTaskbar;
+
+    public MainWindow()
     {
-        private readonly TaskbarIcon _taskbarIcon;
+        _taskbar = CreateTaskbar();
+        InitializeComponent();
+    }
 
-        public MainWindow()
-        {
-            _taskbarIcon = new TaskbarIcon();
-            _taskbarIcon.Icon = System.Drawing.SystemIcons.Application;
-            _taskbarIcon.ToolTipText = "Replay Watcher";
-            _taskbarIcon.DoubleClickCommand = new TreyCommand(ShowWindow); // Добавление обработчика двойного клика
-            InitializeComponent();
-        }
-
-        protected override void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-            CreateContextMenu();
-            Hide();
-        }
-
-        private void ShowWindow()
-        {
-            Show();
-            WindowState = WindowState.Normal;
-        }
-
-        private void Exit()
-        {
-            _taskbarIcon.Dispose();
-            Application.Current.Shutdown();
-        }
-        
-        private void CreateContextMenu()
-        {
-            _taskbarIcon.ContextMenu = new ContextMenu
-            {
-                ItemsSource = new MenuItem[]
-                {
-                    new MenuItem { Header = "Open", Command = new TreyCommand(ShowWindow) },
-                    new MenuItem { Header = "Exit", Command = new TreyCommand(Exit) }
-                }
-            };
-        }
-        
-        protected override void OnStateChanged(EventArgs e)
-        {
-            if (WindowState == WindowState.Minimized)
-            {
-                Hide();
-            }
-            base.OnStateChanged(e);
-        }
-        
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            _taskbarIcon.Dispose();
-            base.OnClosing(e);
-        }
+    protected override void OnInitialized(EventArgs e)
+    {
+        CreateContextMenu();
+        base.OnInitialized(e);
     }
     
-    public class TreyCommand : ICommand
+    private void ShowWindow()
     {
-        private readonly Action _execute;
+        ShowInTaskbar = true;
+        Show();
+        WindowState = WindowState.Normal;
+    }
 
-        public event EventHandler CanExecuteChanged;
+    private void HideWindow()
+    {
+        ShowInTaskbar = false;
+        Hide();
+        WindowState = WindowState.Minimized;
+    }
 
-        public TreyCommand(Action execute)
+    private void ShutDown()
+    {
+        _taskbar.Dispose();
+        Application.Current.Shutdown();
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        if (!Hidden)
         {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            e.Cancel = true;
+            HideWindow();
+            return;
         }
+        _taskbar.Dispose();
+        base.OnClosing(e);
+    }
 
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
+    private TaskbarIcon CreateTaskbar()
+    {
+        var taskbar = new TaskbarIcon();
+        taskbar.Icon = System.Drawing.SystemIcons.Application;
+        taskbar.ToolTipText = "Replay Watcher";
+        taskbar.DoubleClickCommand = new ActionCommand(ShowWindow);
 
-        public void Execute(object parameter)
+        return taskbar;
+    }
+
+    private void CreateContextMenu()
+    {
+        _taskbar.ContextMenu = new ContextMenu
         {
-            _execute();
-        }
+            ItemsSource = new MenuItem[]
+            {
+                new() { Header = "Open", Command = new ActionCommand(ShowWindow) },
+                new() { Header = "Exit", Command = new ActionCommand(ShutDown) }
+            }
+        };
     }
 }
---
+++ ReplayWatcher.Desktop/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ReplayWatcher.Desktop;
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        var provider = services.BuildServiceProvider(new ServiceProviderOptions
+        {
+            ValidateOnBuild = true
+        });
+
+        var app = provider.GetRequiredService<App>();
+        
+        try
+        {
+            app?.Run();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<App>();
+        services.AddSingleton<MainWindow>();
+    }
+}
