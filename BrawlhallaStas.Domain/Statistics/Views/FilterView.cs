53a75550ffe9617db5f5279c4ea88cefcdb2d6c8
kukkich <vitia.iezhovqqqq@gmail.com>
2024-04-08 15:17:03 +0700

    Добавил nullable к полям представления из фильтра. Исправил отображение NAN при дефолтной статистике

--- BrawlhallaStas.Domain/Statistics/Views/FilterView.cs
+++ BrawlhallaStas.Domain/Statistics/Views/FilterView.cs
@@ -9,11 +9,11 @@ public class FilterView
     public DateTime CreatedAt { get; set; }
     public int Wins { get; set; }
     public int Defeats { get; set; }
-    public GameType GameType { get; set; }
-    public int LegendId { get; set; }
-    public int WeaponId { get; set; }
-    public int EnemyLegendId { get; set; }
-    public int EnemyWeaponId { get; set; }
+    public GameType? GameType { get; set; }
+    public int? LegendId { get; set; }
+    public int? WeaponId { get; set; }
+    public int? EnemyLegendId { get; set; }
+    public int? EnemyWeaponId { get; set; }
     public int? TeammateLegendId { get; set; }
     public int? TeammateWeaponId { get; set; }
 }
